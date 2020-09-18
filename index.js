8fe77f59ac05cbb5c9c4fc3e5af472fd0a45dc05
marah <marahabdallah91@gmail.com>
2020-09-18 11:43:22 +0300

    blog app

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
-
 require('dotenv').config();
 const mongoose = require('mongoose');
-const server = require('./lib/server.js');
-
+const server = require('./lib/server');
 
 mongoose.connect(process.env.MONGODB_URI, {
+  useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 
 server.start(process.env.PORT);
